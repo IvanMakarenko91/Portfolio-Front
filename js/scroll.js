507db2684bcbe63248f9abe4bfcb91128d8cbf19
Makarenko Ivan <ivan.makarenko@free.fr>
2022-06-22 15:24:08 +0200

    Footer responsive

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,10 @@
 window.addEventListener("scroll", function() {
   var navMenu = document.getElementById("navM");
   navMenu.classList.toggle("sticky", window.scrollY > 0);
-})
+});
+
+let telechargers = document.getElementById('telecharger');
+let downloads = document.getElementById('download');
+telechargers.addEventListener("click", () => {
+  downloads.style.color = "#52BB32";
+});
